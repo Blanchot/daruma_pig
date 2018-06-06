318adcc318cb2b82dd8bd1787d5dec6545749426
Blanchot <Blanchot@users.noreply.github.com>
2018-06-06 10:33:13 +0200

    Initial commit of luma library test for 7 segment display

---
+++ daruma01.py
@@ -0,0 +1,14 @@
+# daruma01
+# test of luma library
+
+# code below from here: https://media.readthedocs.org/pdf/max7219/stable/max7219.pdf
+from luma.core.interface.serial import spi, noop from luma.core.render import canvas
+from luma.core.virtual import sevensegment
+from luma.led_matrix.device import max7219
+serial = spi(port=0, device=0, gpio=noop())
+device = max7219(serial, cascaded=1)
+seg = sevensegment(device)
+
+# write test
+seg.text = "HELLO"
+
