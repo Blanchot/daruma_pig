e05460a5fc63dda64dd1dfe8d95a80ae571fcabd
Blanchot <Blanchot@users.noreply.github.com>
2018-06-06 10:34:57 +0200

    small bug fix

--- daruma01.py
+++ daruma01.py
@@ -2,7 +2,8 @@
 # test of luma library
 
 # code below from here: https://media.readthedocs.org/pdf/max7219/stable/max7219.pdf
-from luma.core.interface.serial import spi, noop from luma.core.render import canvas
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
 from luma.core.virtual import sevensegment
 from luma.led_matrix.device import max7219
 serial = spi(port=0, device=0, gpio=noop())
