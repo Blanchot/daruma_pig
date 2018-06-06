f057b083ae1a97b9d67979b17b371c681a1236f9
Blanchot <Blanchot@users.noreply.github.com>
2018-06-06 10:49:53 +0200

    temp fix

--- daruma01.py
+++ daruma01.py
@@ -1,6 +1,7 @@
 # daruma01
 # test of luma library
 
+'''
 # code below from here: https://media.readthedocs.org/pdf/max7219/stable/max7219.pdf
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
@@ -9,21 +10,21 @@ from luma.led_matrix.device import max7219
 serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial, cascaded=1)
 seg = sevensegment(device)
-
+'''
 import requests, json
 from time import sleep
 
 # write test
-seg.text = "HELLO"
+#seg.text = "HELLO"
 
 def getBitcoinPrice():
-    URL = 'https://www.bitstamp.net/api/ticker/'
-    try:
-        r = requests.get(URL)
-        priceFloat = float(json.loads(r.text)['last'])
-        return priceFloat
-    except requests.ConnectionError:
-        print("Error querying Bitstamp API")
+  URL = 'https://www.bitstamp.net/api/ticker/'
+  try:
+    r = requests.get(URL)
+    priceFloat = float(json.loads(r.text)['last'])
+    return priceFloat
+  except requests.ConnectionError:
+    print("Error querying Bitstamp API")
 
 while True:
   print("Bitstamp last price: $" + str(getBitcoinPrice()) + "/BTC")
