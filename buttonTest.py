0a7945ec22dea44591275fe9f466ee5a18abfff4
Blanchot <Blanchot@users.noreply.github.com>
2018-06-07 11:51:26 +0200

    Button SHIM testing

---
+++ buttonTest.py
@@ -0,0 +1,10 @@
+#Button Test for Daruma01
+#First test: setting the pixel
+
+# import signal
+import buttonshim
+
+# buttonshim.NAMES = ['A', 'B', 'C', 'D', 'E'] #Can alter these to print the name when pressed?
+
+print('Set Pixel syntax: buttonshim.set_pixel(r,g,b)')
+
