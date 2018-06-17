ea1dc5078e19dd2a423388ac6ed60e30f7d681ef
Blanchot <Blanchot@users.noreply.github.com>
2018-06-17 16:01:13 +0200

    First trial pi code

--- tests/plutus02.py
+++ tests/plutus02.py
@@ -1,6 +1,6 @@
 # Plutus02 Tests
 
-# Luma
+# luma.led_matrix setup
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
 from luma.core.virtual import sevensegment
@@ -9,4 +9,90 @@ serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial, cascaded=2)
 seg = sevensegment(device)
 
+import requests, json
+from time import sleep
+
+neb_amt = 28.741 # NEBL's owned
+prevPrice = 0.0
+
+# API's Used
+URL_1 = 'https://api.binance.com/api/v1/ticker/24hr?symbol=NEBLBTC' #curr. price in BTC + % change last 24hrs
+URL_2 = 'https://api.coinbase.com/v2/prices/spot?currency=EUR' #Convert BTC to Euro
+
+# get current Binance price for NEBL in BTC and percent change over last 24hrs.
+def getNEBL_btc():
+  try:
+    r = requests.get(URL_1)
+    nbBTC = json.loads(r.text)['lastPrice']
+    nbBTC_change24 = json.loads(r.text)['priceChangePercent']
+    
+    #convert to floats
+    nbBTC = float(nbBTC)
+    nbBTC_change24 = float(nbBTC_change24)
+    
+    # CONSOLE PRINTING
+    print('1 NEBL (BTC): ',nbBTC)
+    print('Change (24hrs): {:0.1f}'.format(nbBTC_change24))
+    
+    # Send bitcoin price to euro converter
+    eurPrice = getBTC_euro(nbBTC)
+    
+    # Compile and return text
+    change24 = '{:0.1f}'.format(nbBTC_change24) #str truncated for display
+    
+    return euroPrice, change24
+    
+  except requests.ConnectionError:
+    print("Error querying Binance API")
+
+# convert BTC to Euro, send to changeIndicator, return complete text for display
+def getBTC_euro(nbBTC):
+  try:
+    r = requests.get(URL_2)
+    btcEuro = json.loads(r.text)['data']['amount']
+    nbEUR = float(nbBTC) * float(btcEuro) # NEBL price in EUR
+    print('1 NEBL (EUR): {:0.2f}'.format(nbEUR))
+    
+    '''
+    # Send to changeIndicator rounded to 2 decimal places (for ButtonShim)
+    eurRound = round(nbEUR,2)
+    changeIndicator(eurRound)
+    '''
+    
+    eurPrice = '{:0.2f}'.format(nbEUR) #for display
+    return eurPrice
+    
+  except requests.ConnectionError:
+    print("Error querying Coinbase API")
+
+
+def twin_display():
+  euroPrice, change24 = getNEBL_btc()
+  str1 = '{} {}'.format(eurPrice,change24)
+  str2 = str(round(neb_amt * eurPrice,2))
+  if len(str1) >= 0 and len(str1) <= 10:
+    str1 = '{:_<8s}'.format(str1) # don't know if I need the 's' 
+    print(str1)
+    str2 = '{:<8}'.format(str2)
+    print(str2)
+    total_str = str1 + str2
+    return total_str
+  else:
+    print('str1 too long!')
+    return '--------'
+
+while True:
+  seg.text = twin_display()()
+  print() # separator
+  sleep(60) # checks once a minute
+  #sleep(300) # checks every 5 minutes
+
+'''
+Padding Formatting
+https://pyformat.info/#string_pad_align
+https://www.digitalocean.com/community/tutorials/how-to-use-string-formatters-in-python-3
+'{:10}'.format('test') # with spaces
+'{:_<10}'.format('test') #with underscores
+
+'''
 
